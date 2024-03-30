d16cf725ab43cab15065738be018636b4cfde6d1
SayyehBan <sdvp1991david@gmail.com>
2024-03-31 00:04:19 +0330

    RabbitMQ- پیاده سازی Direct Exchange

---
+++ BasketService/Program.cs
@@ -0,0 +1,14 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+var connection = factory.CreateConnection();
+var channel = connection.CreateModel();
+
+string QueueName = "order.create";
+channel.QueueDeclare(QueueName, false, false, false, null);
+string message = $"Send Shopping cart Information to place an order timr : {DateTime.Now.Ticks}";
+var body = Encoding.UTF8.GetBytes(message);
+channel.BasicPublish(exchange: "", routingKey:QueueName, basicProperties: null,body);
+Console.ReadLine();
---
+++ OrderService/Program.cs
@@ -0,0 +1,20 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+var connection = factory.CreateConnection();
+var channel = connection.CreateModel();
+
+string QueueName = "order.create";
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (sender, args) =>
+{
+    var body = args.Body.ToArray();
+    string message = Encoding.UTF8.GetString(body);
+    Console.WriteLine("Received Message " + message);
+};
+channel.BasicConsume(QueueName, true, consumer);
+
+Console.ReadLine();
