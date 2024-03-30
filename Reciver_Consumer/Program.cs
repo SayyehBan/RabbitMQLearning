4b023d952d1065239b60eaa5bd98c36fbd8708ea
SayyehBan <sdvp1991david@gmail.com>
2024-03-30 17:38:10 +0330

    ارسال و دریافت بین RabbitMQ

--- Reciver_Consumer/Program.cs
+++ Reciver_Consumer/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+var connection = factory.CreateConnection();
+var channel = connection.CreateModel();
+string QueueName = "zljn7vcbl3t28usw";
+channel.QueueDeclare(QueueName, false, false, false, null);
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (model, args) =>
+{
+    var body = args.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine("Received Message " + message);
+    Thread.Sleep(2000);
+};
+channel.BasicConsume(QueueName, true, consumer);
+
+Console.ReadLine();
--- Sender_Producer/Program.cs
+++ Sender_Producer/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RabbitMQ.Client;
+using SayyehBanTools.GenerateValue;
+using System.Text;
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+var connection = factory.CreateConnection();
+var channel = connection.CreateModel();
+string QueueName = "zljn7vcbl3t28usw";
+channel.QueueDeclare(QueueName, false, false, false, null);
+for (int i = 0; i < 30; i++)
+{
+    string message = $"This is a test meassage from my sender at :{DateTime.Now.Ticks}";
+    var body = Encoding.UTF8.GetBytes(message);
+    channel.BasicPublish("", QueueName, null, body);
+}
+
+
+
+channel.Close();
+connection.Close();
+Console.ReadLine();
